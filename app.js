7f1ea70e9376449a98b32a125f7d89bed5cd762a
Shubham <shubhamkumar81866@gmail.com>
2022-07-06 17:35:57 +0530

    minor changes

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get('/form', (req, res) => {
 });
 
 app.use(express.static(__dirname));
-app.use('*',(req,res)=>{
+app.get('*',(req,res)=>{
     res.status(404).sendFile(__dirname + "/public/error/index.html");
 });
 
